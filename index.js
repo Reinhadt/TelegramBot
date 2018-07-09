5d23256dcb62f99bcd75c71f7b6ad8028052ace3
unknown <omavaloss@gmail.com>
2018-07-09 16:38:31 -0500

    Ahora manda el clima y una imagencita

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
+process.env["NTBA_FIX_319"] = 1;
+
 const TelegramBot = require('node-telegram-bot-api')
+const request= require('request')
 require('dotenv').config()
 
-const token = process.env.TELEGRAM-KEY
-const weatherToken = process.env.WEATHER_KEY;
+const token = process.env.TELEGRAM_KEY
+const weatherToken = process.env.WEATHER_KEY
+
 
 const bot = new TelegramBot(token, {polling: true})
 
-bot.on('message', (msg) => {
-    var hi = "hola";
-    if(msg.text.toString().toLowerCase().indexOf(hi)===0){
-        bot.sendMessage(msg.chat.id, `Hola ${msg.from.first_name}`);
-    }
-})
+function getDatos(datos){
+    let d = JSON.parse(datos);
 
-'230c6f8418242ce57fbfae6b311a1f75'
+    let imgStream = request.get(`http://openweathermap.org/img/w/${d.weather[0].icon}.png`)
+
+    return {
+        info: `${d.main.temp}°C`,
+        imagen: imgStream
+    }
+}
+
+bot.on('message', (msg) => {
+
+    //obtengo el texto y lo paso a lowercase
+    let texto = msg.text.toString().toLowerCase()
+    //limpio acentos y cosas así
+    let cleaned = texto.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    //url para hacer petición
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cleaned}&appid=${weatherToken}&units=metric`;
+
+    const fileOptions = {
+        height: 50,
+        width: 50
+    }
+
+    //request a la api
+    return request(url, (err, resp, body)=>{
+        let result = getDatos(body);
+        bot.sendPhoto(msg.chat.id, result.imagen,{caption: result.info}, {}, fileOptions, {parse_mode: "HTML"})
+    })
+
+})
